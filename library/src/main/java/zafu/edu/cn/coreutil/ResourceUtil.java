417276cd19bad6315633b48bcd187b1bb56f2335
lizhangqu <zhangqu.li@kltz88.com>
2015-08-04 15:53:41 +0800

    增加通过资源名获得资源id

--- library/src/main/java/zafu/edu/cn/coreutil/ResourceUtil.java
+++ library/src/main/java/zafu/edu/cn/coreutil/ResourceUtil.java
@@ -1,6 +1,8 @@
 package zafu.edu.cn.coreutil;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 
 import java.io.IOException;
@@ -14,6 +16,41 @@ import java.io.UnsupportedEncodingException;
  * Time: 17:33
  */
 public class ResourceUtil {
+    public static final String LAYTOUT="layout";
+    public static final String DRAWABLE="drawable";
+    public static final String MIPMAP="mipmap";
+    public static final String MENU="menu";
+    public static final String RAW="raw";
+    public static final String ANIM="anim";
+    public static final String STRING="string";
+    public static final String STYLE="style";
+    public static final String STYLEABLE="styleable";
+    public static final String INTEGER="integer";
+    public static final String ID="id";
+    public static final String DIMEN="dimen";
+    public static final String COLOR="color";
+    public static final String BOOL="bool";
+    public static final String ATTR="attr";
+    //TODO please add other strings by yourself
+
+    /**
+     * 根据资源名获得资源id
+     * @param context 上下文
+     * @param name 资源名
+     * @param type 资源类型
+     * @return 资源id，找不到返回0
+     */
+    public static int getResourceId(Context context,String name,String type){
+        Resources resources=null;
+        PackageManager pm=context.getPackageManager();
+        try {
+            resources=context.getResources();
+            return resources.getIdentifier(name, type, context.getPackageName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
     /**
      * 从assets目录下读取文件内容
      *
