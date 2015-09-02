76127bd1203018e3e70e46e7ec2c1a1868dd1007
lizhangqu <zhangqu.li@kltz88.com>
2015-09-02 13:50:41 +0800

    双向认证支持

--- library/src/main/java/zafu/edu/cn/coreutil/SSLUtil.java
+++ library/src/main/java/zafu/edu/cn/coreutil/SSLUtil.java
@@ -3,9 +3,15 @@ package zafu.edu.cn.coreutil;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
@@ -18,12 +24,24 @@ import javax.net.ssl.TrustManagerFactory;
  */
 public class SSLUtil {
     //使用命令keytool -printcert -rfc -file srca.cer 导出证书为字符串，然后将字符串转换为输入流，如果使用的是okhttp可以直接使用new Buffer().writeUtf8(s).inputStream()
+
     /**
      * 返回SSLSocketFactory
+     *
      * @param certificates 证书的输入流
      * @return SSLSocketFactory
      */
     public static SSLSocketFactory getSSLSocketFactory(InputStream... certificates) {
+        return getSSLSocketFactory(null,certificates);
+    }
+
+    /**
+     * 双向认证
+     * @param keyManagers KeyManager[]
+     * @param certificates 证书的输入流
+     * @return SSLSocketFactory
+     */
+    public static SSLSocketFactory getSSLSocketFactory(KeyManager[] keyManagers, InputStream... certificates) {
         try {
             CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
             KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
@@ -41,10 +59,39 @@ public class SSLUtil {
             SSLContext sslContext = SSLContext.getInstance("TLS");
             TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             trustManagerFactory.init(keyStore);
-            sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
+            sslContext.init(keyManagers, trustManagerFactory.getTrustManagers(), new SecureRandom());
             SSLSocketFactory socketFactory = sslContext.getSocketFactory();
             return socketFactory;
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获得双向认证所需的参数
+     * @param bks bks证书的输入流
+     * @param keystorePass 秘钥
+     * @return KeyManager[]对象
+     */
+    public static KeyManager[] getKeyManagers(InputStream bks, String keystorePass) {
+        KeyStore clientKeyStore = null;
+        try {
+            clientKeyStore = KeyStore.getInstance("BKS");
+            clientKeyStore.load(bks, keystorePass.toCharArray());
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(clientKeyStore, keystorePass.toCharArray());
+            KeyManager[] keyManagers = keyManagerFactory.getKeyManagers();
+            return keyManagers;
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
